17328f0727953740ce154077324e9617997c762a
Yure <yure.nathan303@gmail.com>
2025-11-20 23:05:10 -0300

    chore: adicionar estrutura inicial do projeto

---
+++ backend/apiChatSimples/src/test/java/com/lifty/apiChatSimples/ApiChatSimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifty.apiChatSimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiChatSimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
