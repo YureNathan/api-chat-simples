10d00b1cb3740d918b2613030451a4508eb18616
Yure <yure.nathan303@gmail.com>
2025-11-21 01:35:43 -0300

    refactor(architecture): reorganizar classes em controller/service/repository

--- backend/apiChatSimples/src/main/java/com/lifty/apiChatSimples/controller/UserController.java
+++ backend/apiChatSimples/src/main/java/com/lifty/apiChatSimples/controller/UserController.java
@@ -1,34 +1,11 @@
 package com.lifty.apiChatSimples.controller;
 
-import com.lifty.apiChatSimples.dtos.UserRequestDTO;
-import com.lifty.apiChatSimples.dtos.UserResponseDTO;
-import com.lifty.apiChatSimples.service.UserService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
-
+@RequestMapping
 public class UserController {
 
-    public final UserService userService;
-
-    public UserController(UserService userService){
-        this.userService = userService;
-    }
-
-   @PostMapping
-    public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserRequestDTO userRequestDTO) {
-       UserResponseDTO userResponseDTO = userService.createUser(userRequestDTO);
-       return ResponseEntity.ok(userResponseDTO);
-   }
-
-   @GetMapping
-    public ResponseEntity<List<UserResponseDTO>> listAllUser(){
-      List<UserResponseDTO> userResponseDTOS = userService.listAllUser();
-      return ResponseEntity.ok(userResponseDTOS);
-   }
 
 }
