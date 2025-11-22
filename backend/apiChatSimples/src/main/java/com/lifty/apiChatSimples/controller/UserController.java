14855e3995967f2757ed20f99f7668947db71cb7
Yure <yure.nathan303@gmail.com>
2025-11-22 18:24:33 -0300

    feat(Usercontroller): adiciona método atualizar usuário

---
+++ backend/apiChatSimples/src/main/java/com/lifty/apiChatSimples/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.lifty.apiChatSimples.controller;
+
+import com.lifty.apiChatSimples.dtos.user.UserRequestDTO;
+import com.lifty.apiChatSimples.dtos.user.UserResponseDTO;
+import com.lifty.apiChatSimples.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+
+public class UserController {
+
+    public final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+   @PostMapping
+    public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserRequestDTO userRequestDTO) {
+       UserResponseDTO userResponseDTO = userService.createUser(userRequestDTO);
+       return ResponseEntity.ok(userResponseDTO);
+   }
+
+   @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> listAllUser(){
+      List<UserResponseDTO> userResponseDTOS = userService.listAllUser();
+      return ResponseEntity.ok(userResponseDTOS);
+   }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> listUserId(@PathVariable Long id){
+        UserResponseDTO userResponseDTO = userService.listUserId(id);
+        return ResponseEntity.ok(userResponseDTO);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateUser(@PathVariable Long id, @RequestBody UserRequestDTO userRequestDTO){
+        userService.updateUser(id, userRequestDTO);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
