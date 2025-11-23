54c6b9e34fdbe3f3f768b78b5e8f236688be025d
Yure <yure.nathan303@gmail.com>
2025-11-23 08:58:51 -0300

    refactor(Mensage-Ententy): reorganiza atributos e define novo atributo de conteúdo.

---
+++ backend/apiChatSimples/src/main/java/com/lifty/apiChatSimples/entity/Mensage.java
@@ -0,0 +1,83 @@
+package com.lifty.apiChatSimples.entity;
+
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "mensages")
+
+public class Mensage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_conversation")
+    private Conversation conversation;
+
+    @ManyToOne
+    @JoinColumn(name = "id_sender")
+    private User idSender;
+
+    @ManyToOne
+    @JoinColumn(name = "id_recipient")
+    private  User idRecipient;
+
+    public User getIdRecipient() {
+        return idRecipient;
+    }
+
+    public void setIdRecipient(User idRecipient) {
+        this.idRecipient = idRecipient;
+    }
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "date_time", nullable = false)
+    @CreationTimestamp
+    private LocalDateTime sentAt;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Conversation getIdConversation() {
+        return conversation;
+    }
+
+    public void setIdConversation(Conversation idConversation) {
+        this.conversation = idConversation;
+    }
+
+    public User getIdSender() {
+        return idSender;
+    }
+
+    public void setIdSender(User idSender) {
+        this.idSender = idSender;
+    }
+
+    public LocalDateTime getSentAt() {
+        return sentAt;
+    }
+
+    public void setSentAt(LocalDateTime sentAt) {
+        this.sentAt = sentAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
